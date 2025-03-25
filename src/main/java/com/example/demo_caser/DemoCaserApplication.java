699e518ea6f0e69c13989b4ee327ef6dcd0e9e7d
kazu <test@gmail.com>
2025-03-25 12:53:13 +0900

    hello

---
+++ src/main/java/com/example/demo_caser/DemoCaserApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo_caser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCaserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCaserApplication.class, args);
+		
+	}
+
+}
